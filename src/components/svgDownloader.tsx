34465860d326d55d3b32991497a0b9b2ceb374a6
Blankjr <blankj.me@gmail.com>
2022-09-10 16:54:51 +0200

    Feature: Download Button (#1)

---
+++ src/components/svgDownloader.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { useCallback, useRef } from "react";
+import QRCode from "react-qr-code";
+
+function downloadBlob(blob: Blob, filename: string) {
+  const objectUrl = URL.createObjectURL(blob);
+
+  const link = document.createElement("a");
+  link.href = objectUrl;
+  link.download = filename;
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+
+  setTimeout(() => URL.revokeObjectURL(objectUrl), 5000);
+}
+
+export default function SvgDownloader(props: any) {
+  const svgRef = useRef(null);
+  const downloadSVG = useCallback(() => {
+    if (svgRef.current) {
+      const svg = svgRef.current.innerHTML;
+      const blob = new Blob([svg], { type: "image/svg+xml" });
+      downloadBlob(blob, `myimage.svg`);
+    }
+  }, []);
+  return (
+    <>
+      <div ref={svgRef}>
+        <QRCode className="m-4" value={props.qrCodeValue} bgColor={props.backgroundColor} fgColor={props.foregroundColor} />
+      </div>
+      <button className="mt-2 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded" onClick={downloadSVG} >
+        Download as SVG
+      </button>
+    </>
+  )
+}
+
